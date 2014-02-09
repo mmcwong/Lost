bfaad2143e05d340f93a84bbc6ca7c3e07e432b4
Michelle <michelle_wong47@hotmail.com>
2014-02-09 15:09:54 -0800

    first commit

---
+++ Lost/Lost/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+namespace Lost
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private Label label1;
+
+    }
+}
+
---
+++ Lost/Lost/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Device.Location;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Lost.Services;
+using System.Net;
+using System.Timers;
+
+namespace Lost
+{
+
+    public partial class Form1 : Form
+    {
+        private GeoCoordinateWatcher gcw;
+        private SimpleService sv;
+        private System.Timers.Timer t;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.notifyIcon1.Icon = new Icon(SystemIcons.WinLogo, 40, 40);
+            this.notifyIcon1.MouseDoubleClick += notifyIcon1_MouseDoubleClick; 
+            this.Resize +=Form1_Resize;
+
+            String user, pwd;
+            ReadPassword(out user, out pwd);
+
+            sv = new SimpleService();
+            NetworkCredential credentials = new NetworkCredential(user, pwd);
+            sv.Credentials = credentials;
+            sv.PreAuthenticate = true;
+
+            t = new System.Timers.Timer(1000);
+            t.Elapsed += SendLocation;
+
+            gcw = new GeoCoordinateWatcher();
+            gcw.Start();
+            gcw.StatusChanged += gcw_StatusChanged;
+        }
+
+        private void ReadPassword(out string user, out string pwd)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"Credentials.txt");
+            user = lines[0];
+            pwd = lines[1];
+        }
+
+        void Form1_Resize(object sender, EventArgs e)
+        {             
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.ShowInTaskbar = false;
+                this.Hide();
+            }
+        }
+
+        void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.ShowInTaskbar = true;
+        }
+                
+        void gcw_StatusChanged(object sender, GeoPositionStatusChangedEventArgs e)
+        {
+            if (e.Status == GeoPositionStatus.Ready)
+                t.Start();
+        }
+
+        protected override void SetVisibleCore(bool value)
+        {
+            if (!this.IsHandleCreated)
+            {
+                value = false;
+                this.CreateHandle();
+            }
+            base.SetVisibleCore(value);
+        }
+
+        private void SendLocation(object sender, ElapsedEventArgs e)
+        {
+            try
+            {
+                GeoCoordinate gc = gcw.Position.Location;
+                Location loc = new Location();
+                loc.Latitude = gc.Latitude;
+                loc.Longitude = gc.Longitude;
+
+                string stResponse = sv.ProcessMyLocation(loc);
+                Console.WriteLine(stResponse);
+
+                if (stResponse == "Lock Computer")
+                    LockComputer();
+            }
+            catch
+            {               
+            }
+                                 
+        }
+
+        private void LockComputer()
+        {
+            System.Diagnostics.Process.Start(@"C:\WINDOWS\system32\rundll32.exe", "user32.dll,LockWorkStation");
+        }
+
+
+    }
+}
